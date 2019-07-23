aeda005f1bd30d54ae58147c84d63eb6060d096d
Shahwar Spall <shevy.spall@gmail.com>
2019-07-23 01:38:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -2,9 +2,19 @@ def create_an_empty_array
   create_an_empty_array = []
 end
 
+<<<<<<< HEAD
 
 def create_an_array
   ["Shahwar","Hamzah","Haroon","Jia"]
+=======
+create_an_empty_array
+
+
+
+def create_an_array
+  create_an_array.size
+  
+>>>>>>> 0f8b990fd0fd0ef7630fb271b11b9c2f4919836f
 end
 
 
@@ -25,7 +35,11 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
+<<<<<<< HEAD
   array[index_number]
+=======
+  array[]
+>>>>>>> 0f8b990fd0fd0ef7630fb271b11b9c2f4919836f
 end
 
 def retrieve_first_element_from_array(array)
